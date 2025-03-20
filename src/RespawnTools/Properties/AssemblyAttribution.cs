b9db74cf62b465a88c6474eb4f5a13b11e9475a2
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-20 23:49:48 +0000

    fix: compatibility issues with new logger

--- src/RespawnTools/Properties/AssemblyAttribution.cs
+++ src/RespawnTools/Properties/AssemblyAttribution.cs
@@ -2,14 +2,14 @@
 
 // ReSharper disable StringLiteralTypo
 
-[assembly: ModDependency("game", "1.20.1")]
+[assembly: ModDependency("game", "1.20.6")]
 
 [assembly:ModInfo(
     "Respawn Tools",
     "respawntools",
     Description = "Adds tools for servers, to alter the consequences of death, and respawning.",
     Side = "Universal",
-    Version = "1.4.2",
+    Version = "1.4.3",
     NetworkVersion = "1.0.0",
     IconPath = "modicon.png",
     Website = "https://apachetech.co.uk",
